584d693a913fdcc834e0800b10caaa7d76bf323a
Rohil-Ali <132683375+Rohil-Ali@users.noreply.github.com>
2025-09-25 18:43:48 +0100

    initialize project structure and add dependencies
    initialize project structure and add dependencies
    
    - Added dotenv, express, open, and spotify-web-api-node to package.json dependencies.
    - Created initial HTML structure for the music player application.

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
 const path = require('path');
+const { app, BrowserWindow } = require('electron');
+const SpotifyWebApi = require('spotify-web-api-node');
+const express = require('express');
+const open = require('open');
+require('dotenv').config();
 
+//Electron Reload for renderer process
 require('electron-reload')(__dirname, {
   electron: path.join(__dirname, 'node_modules', '.bin', 'electron'),
   hardResetMethod: 'exit'
 });
 
+//Spotify API setup
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  redirectUri: process.env.SPOTIFY_REDIRECT_URI  // redirect for OAuth
+});
 
-const { app, BrowserWindow } = require('electron')
+const scopes = ['streaming', 'user-read-playback-state', 'user-modify-playback-state'];
 
+//Main window
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 380,
     height: 600
   })
 
-  win.loadFile('index.html')
+  win.loadFile('html/index.html')
 }
 
 app.whenReady().then(() => {
